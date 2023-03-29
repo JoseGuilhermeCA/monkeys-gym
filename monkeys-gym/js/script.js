2d9714367092467f6b32a96e7bd89870ae14292b
JoseGuilhermeCA <gui.jgca@gmail.com>
2023-03-29 17:46:29 -0300

    FORMS DONE

--- monkeys-gym/js/script.js
+++ monkeys-gym/js/script.js
@@ -1,28 +1,33 @@
 
 var inputQtde = document.querySelector('#qtde')
 inputQtde.addEventListener("change", atualizarOrcamento)
-document.querySelector('#js').addEventListener("change", atualizarOrcamento)
+document.querySelector('#Veiculo').addEventListener("change", atualizarOrcamento)
 document.querySelector('#layout_sim').addEventListener("change", atualizarOrcamento)
 document.querySelector('#layout_nao').addEventListener("change", atualizarOrcamento)
-document.querySelector('#prazo').addEventListener("change", atualizarOrcamento)
+
 
 function atualizarOrcamento(){
     let qtde = inputQtde.value 
-    let preco = qtde * 100
+    let plano = ''
     
-    let js = document.querySelector('#js').checked
-    if(js) preco *= 1.1
+    let Veiculo = document.querySelector('#Veiculo').checked
+    if(Veiculo) plano = 'PREÇO TRIMESTRAL'
 
     let layout = document.querySelector("#layout_sim").checked
-    if(layout) preco *= 1 + (qtde * .1)
+    if(layout) plano = 'PLANO SEMESTRAL'
 
-    let prazo = document.querySelector("#prazo").value
-    let labelPrazo = document.querySelector("label[for=prazo]")
-    labelPrazo.innerHTML = `Prazo (${prazo} semanas)` 
-    let taxaDeUrgencia = 1 - (prazo * .01);
-    preco *= taxaDeUrgencia
+    if(Veiculo == true &&  layout == true){
+        plano = 'PLANO SEMESTRAL'
+    } else if(Veiculo == true){
+        plano = 'PLANO SEMESTRAL'
+    }
+    else if(layout == true){
+        plano = 'PLANO TRIMESTRAL'
+    } else{
+        plano = 'PLANO MENSAL'
+    }
 
     let output = document.querySelector('#secao-orcamento form output')
-    output.innerHTML = "R$ " + preco.toFixed(2)
-    console.log(js)
+    output.innerHTML =  plano
+    console.log(Veiculo)
 }
